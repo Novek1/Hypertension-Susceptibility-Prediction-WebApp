fcede2a3cd5317e1c65fa3331e2f4c3490c402ef
Kevon Bvunza <117343733+Novek1@users.noreply.github.com>
2025-05-24 12:41:28 +0200

    Update Update streamlit_app.py

--- Update streamlit_app.py
+++ Update streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd 
+import numpy as np
 
 st.title('💊🩸 Hypertension Susceptibility Prediction WebApp')
 
 st.info("This web application predicts an individual's risk of developing hypertension using machine learning models. By analyzing key health indicators like age, BMI, blood pressure, glucose levels, and lifestyle factors (e.g., smoking, diabetes, medication use), the app provides an instant risk assessment.")
+df= pd.read_csv("/workspaces/Hypertension-Susceptibility-Prediction-WebApp/Hypertension_Cleaned(2).csv")
+df=df.drop('Unnamed: 0', axis=1)# delete unwanted index column
+df= df.drop('Risk', axis=1) # delete the binary risk column
+df
