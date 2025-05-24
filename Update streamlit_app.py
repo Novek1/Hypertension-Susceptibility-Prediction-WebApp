0ca6736db6782b90fb1602d982f0fa48d805b71c
Kevon Bvunza <117343733+Novek1@users.noreply.github.com>
2025-05-24 16:26:12 +0200

    Update Update streamlit_app.py

--- Update streamlit_app.py
+++ Update streamlit_app.py
@@ -6,6 +6,7 @@ st.title('💊🩸 Hypertension Susceptibility Prediction WebApp')
 
 st.info("This web application predicts an individual's risk of developing hypertension using machine learning models. By analyzing key health indicators like age, BMI, blood pressure, glucose levels, and lifestyle factors (e.g., smoking, diabetes, medication use), the app provides an instant risk assessment.")
 
+st.write("Please enter the patience information and medical measurements below :")
 
 # Load model and scaler
 model = joblib.load("svm_model.pkl")
