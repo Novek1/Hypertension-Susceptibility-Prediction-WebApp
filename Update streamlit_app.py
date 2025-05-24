ffab1a0ef41bd2502776add5be58925761d23f82
Kevon Bvunza <117343733+Novek1@users.noreply.github.com>
2025-05-24 12:24:53 +0200

    Update Update streamlit_app.py

--- Update streamlit_app.py
+++ Update streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('💊🩸 Hypertension Susceptibility Prediction WebApp by Kevon Bvunza')
 
-st.info('This web application predicts an individual's risk of developing hypertension using machine learning models. By analyzing key health indicators like age, BMI, blood pressure, glucose levels, and lifestyle factors (e.g., smoking, diabetes, medication use), the app provides an instant risk assessment.')
+st.info("This web application predicts an individual's risk of developing hypertension using machine learning models. By analyzing key health indicators like age, BMI, blood pressure, glucose levels, and lifestyle factors (e.g., smoking, diabetes, medication use), the app provides an instant risk assessment.")
